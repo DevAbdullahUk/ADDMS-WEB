f90eb3d83031ea29186af5d22943871c2d107955
Abdullah-Janjua <AbdullahJanjuauk@gmail.com>
2018-03-14 20:08:48 +0300

    Not Finish

--- homePage_myTrips_overAll.js
+++ homePage_myTrips_overAll.js
@@ -7,15 +7,25 @@ $(".DRIVING_TIPS_TOP_BAR").click(
   function(){
 $(this).addClass("SELECTED_TOP_BAR");
 $(".OVER_ALL_TOP_BAR").removeClass("SELECTED_TOP_BAR");
+  
+    ChageDivs("#OVERALL_VIEW_DIV","#DRIVING_TIPS_DIV");
+    
 } );
   
   $(".OVER_ALL_TOP_BAR").click(
   function(){
 $(this).addClass("SELECTED_TOP_BAR");
 $(".DRIVING_TIPS_TOP_BAR").removeClass("SELECTED_TOP_BAR");
-    
+  
+   ChageDivs("#DRIVING_TIPS_DIV","#OVERALL_VIEW_DIV");
     
 } );
+  
+  function ChageDivs (hide,show){
+    $(hide).hide();
+    $(show).show();
+  }
+  
   
   function addTrip (Day, Month, dayNumber, dayText, Alerts, Speed, Time){
     document.getElementById("OUTER_OVERALL").innerHTML += ' <div class="Inner_OVERALL"> <span class="date"> <strong>'+Day+'</strong><br>'+Month+'<br>'+dayNumber+' <br> </span> <div class="trips"> <p>'+dayText+'</p><table> <tr> <td class="number">'+Alerts+'</td> <td>Number<br> of alerts</td> <td class="number">'+Speed+'</td><td>Average<br>speed (km/h)</td> <td class="number">'+Time+'</td> <td>Trip total <br> time (hrs)</td> </tr> </table </div> </div>';
---
+++ monitoring_.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    var selectedElm;
+    
+$(".name").click(
+    
+    function(event){
+    $(this).css({
+    'background-color':'#D9D9D9',
+    'font-weight': 'bold'
+    });
+        
+    if (selectedElm != undefined && selectedElm != this){
+     // Not the first run (something is selected)
+    $(selectedElm).css({
+    'background-color':'#EFEFEF',
+    'font-weight': '100'
+    });
+     }
+    
+     selectedElm = this;
+});
+
+     
+    
+});
+
+    function initMap() {
+        var uluru = {lat: 24.66563, lng: 46.67630};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    
